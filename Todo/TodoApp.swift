e2ac5e9e73aabe5a46ea4a6828eeb8a8f7036982
ujeon jang <wj9304@gmail.com>
2022-07-16 17:59:51 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 우전 on 2022/07/16.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
